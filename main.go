741874cadd3b00151def47208dece3d337a293d2
nargok <nargok@gmail.com>
2019-04-06 16:55:49 +0900

    split into several files

--- main.go
+++ main.go
@@ -1,49 +1,13 @@
 package main
 
 import(
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"time"
-
-	"github.com/gorilla/mux"
 )
-
-
-type Todo struct {
-	Name      string	`json:"name"`
-	Completed bool		`json:"completed"`
-	Due 	  time.Time	`json:"due"`
-}
-
-type Todos []Todo
 
 func main() {
 
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
-	router.HandleFunc("/todos", TodoIndex)
-	router.HandleFunc("/todos/{todoId}", TodoShow)
+	router := NewRouter()
 
 	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome!")
-}
-
-func TodoIndex(w http.ResponseWriter, r *http.Request)  {
-	todos := Todos{
-		Todo{Name: "Write presentation" },
-		Todo{Name: "Host meetup" },
-	}
-
-	json.NewEncoder(w).Encode(todos)
-}
-
-func TodoShow(w http.ResponseWriter, r *http.Request)  {
-	vars := mux.Vars(r)
-	todoId := vars["todoId"]
-	fmt.Fprint(w, "Todo show:", todoId)
 }
